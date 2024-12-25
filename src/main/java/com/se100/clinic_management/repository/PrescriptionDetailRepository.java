47362562fa4911f09b82650deac85ade5d03fb8b
NguyenIsHere <22520988@gm.uit.edu.vn>
2024-12-25 21:36:09 +0700

    fix id, typo

--- src/main/java/com/se100/clinic_management/repository/PrescriptionDetailRepository.java
+++ src/main/java/com/se100/clinic_management/repository/PrescriptionDetailRepository.java
@@ -1,7 +1,8 @@
 package com.se100.clinic_management.repository;
 
+import com.se100.clinic_management.dto.PrescriptionDetailId;
 import com.se100.clinic_management.model.PrescriptionDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PrescriptionDetailRepository extends JpaRepository<PrescriptionDetail, Integer> {
+public interface PrescriptionDetailRepository extends JpaRepository<PrescriptionDetail, PrescriptionDetailId> {
 }
