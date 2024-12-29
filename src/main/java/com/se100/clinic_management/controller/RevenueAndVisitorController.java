c57ed092eecd183704efb537aed911b125cd0178
NguyenIsHere <22520988@gm.uit.edu.vn>
2024-12-30 01:32:12 +0700

    add revenue and visitor controller

--- src/main/java/com/se100/clinic_management/controller/RevenueAndVisitorController.java
+++ src/main/java/com/se100/clinic_management/controller/RevenueAndVisitorController.java
@@ -1,5 +1,39 @@
 package com.se100.clinic_management.controller;
 
-public class RevenueAndVisitorController {
+import com.se100.clinic_management.service.RevenueAndVisitorServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+@RestController
+@RequestMapping("/api/statistics")
+public class RevenueAndVisitorController {
+  @Autowired
+  private RevenueAndVisitorServiceImpl revenueAndVisitorService;
+
+  @GetMapping("/revenue")
+  public Float getRevenue(
+      @RequestParam("startDate") String startDateStr,
+      @RequestParam("endDate") String endDateStr) {
+    LocalDate startDate = LocalDate.parse(startDateStr);
+    LocalDate endDate = LocalDate.parse(endDateStr);
+    return revenueAndVisitorService.calculateRevenue(startDate, endDate);
+  }
+
+  @GetMapping("/visitors")
+  public int getVisitors(
+      @RequestParam("startDate") String startDateStr,
+      @RequestParam("endDate") String endDateStr) {
+    LocalDate startDate = LocalDate.parse(startDateStr);
+    LocalDate endDate = LocalDate.parse(endDateStr);
+    return revenueAndVisitorService.countVisitors(startDate, endDate);
+  }
+
+  @ExceptionHandler(DateTimeParseException.class)
+  public ResponseEntity<String> handleDateTimeParseException(DateTimeParseException ex) {
+    return ResponseEntity.badRequest().body("Ngày không đúng định dạng, vui lòng sử dụng định dạng yyyy-MM-dd.");
+  }
 }
--- src/main/java/com/se100/clinic_management/repository/PatientRepository.java
+++ src/main/java/com/se100/clinic_management/repository/PatientRepository.java
@@ -1,10 +1,12 @@
 package com.se100.clinic_management.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.se100.clinic_management.model.Patient;
 
 public interface PatientRepository extends JpaRepository<Patient, Integer>, JpaSpecificationExecutor<Patient> {
-
+  int countByCreateAtBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
